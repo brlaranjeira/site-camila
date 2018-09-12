a49d085ce271fd47d64ad6419da63c12daff1c19
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-09-12 19:08:56 -0300

    adiciona form de contato, ainda em arquivo teste

---
+++ ajax/mail.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brlaranjeira
+ * Date: 12/09/18
+ * Time: 18:44
+ */
+
+echo "foi";
---
+++ js/index.js
@@ -0,0 +1,15 @@
+const enviaContato = () => {
+    const nome = document.getElementById('contato-nome').value;
+    const mail = document.getElementById('contato-email').value;
+    const fone = document.getElementById('contato-fone').value;
+    const msg = document.getElementById('contato-mensagem').value;
+    $.ajax('../ajax/mail.php',{
+        method: 'post',
+        data: { nome: nome, mail: mail, fone: fone, msg: msg},
+        success: () => {
+            alert('foifoi');
+        }, error: () => {
+            alert('naaaaao');
+        }
+    });
+}
