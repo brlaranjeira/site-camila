e212ec7717e19069e0ad8098c24cc8e74814e86b
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2018-11-20 13:57:12 -0200

    coloca form de contato na tela

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
+$('.input-fone').mask('(00) 000-000-000');
+
 const enviaContato = () => {
     const nome = document.getElementById('contato-nome').value;
-    const mail = document.getElementById('contato-email').value;
+    const mail = document.getElementById('contato-mail').value;
     const fone = document.getElementById('contato-fone').value;
-    const msg = document.getElementById('contato-mensagem').value;
+    const msg = document.getElementById('contato-msg').value;
+    debugger;
     $.ajax('../ajax/mail.php',{
         method: 'post',
         data: { nome: nome, mail: mail, fone: fone, msg: msg},
