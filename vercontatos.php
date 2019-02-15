5e7003a2414cb6044590610c36df246e36aa63f8
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2019-02-15 15:00:23 -0200

    mostra contatos em ordem de data e hora

--- vercontatos.php
+++ vercontatos.php
@@ -5,6 +5,21 @@
  * Date: 15/02/19
  * Time: 13:34
  */
+
+
+function getOutContent($f) {
+    $dia = substr($f,0,10);
+    $campos = explode('-',$dia);
+    $dia = $campos[2] . '/' . $campos[1] . '/' . $campos[0];
+    $hora = substr($f,11,8);
+    $hora = str_replace('_',':',$hora);
+    echo "Dia $dia as $hora<br/>";
+    $fcontent =  file_get_contents('./contato/'.$f);
+    $fcontent = nl2br($fcontent);
+    echo $fcontent . '<br/>';
+    echo '<hr/>';
+}
+
 $ok = false;
 if (isset($_POST['pw'])) {
     $h = hash('sha256', $_POST['pw']);
@@ -12,21 +27,18 @@ if (isset($_POST['pw'])) {
         $ok = true;
         $diretorio = opendir('./contato/');
         $fname = readdir($diretorio);
+        $arquivos = [];
         while ($fname !== false) {
             if ($fname !== '.' && $fname !== '..') {
                 //echo $fname . '<br/>';
-                $dia = substr($fname,0,10);
-                $campos = explode('-',$dia);
-                $dia = $campos[2] . '/' . $campos[1] . '/' . $campos[0];
-                $hora = substr($fname,11,8);
-                $hora = str_replace('_',':',$hora);
-                echo "Dia $dia as $hora<br/>";
-                $fcontent =  file_get_contents('./contato/'.$fname);
-                $fcontent = nl2br($fcontent);
-                echo $fcontent . '<br/>';
-                echo '<hr/>';
+                $arquivos[] = $fname;
             }
             $fname = readdir($diretorio);
+        }
+        sort($arquivos,SORT_STRING);
+        $arquivos = array_reverse($arquivos);
+        foreach ($arquivos as $arquivo) {
+            getOutContent($arquivo);
         }
     }
 }
