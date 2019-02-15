d6120696e52cc3bb7237125767797e5b7b68f9c3
Bruno Rezende Laranjeira <brlaranjeira@gmail.com>
2019-02-15 08:59:43 -0200

    adiciona email

--- ajax/mail.php
+++ ajax/mail.php
@@ -22,7 +22,7 @@ Nome: $contatoNome\r\n
 E-mail: $contatoMail\r\n
 Telefone: $contatoFone\r\n
 Mensagem: $contatoMsg";
-$mailOk = mail ( "brlaranjeira@gmail.com" , "MENSAGEM NO SITE" , $str);
+$mailOk = mail ( "brlaranjeira@gmail.com, psi_camila@hotmail.com" , "MENSAGEM NO SITE" , $str);
 $msg = $mailOk ?
     "Olá, $contatoNome<br/>Obrigada por entrar em contato.<br/>Em breve entrarei em contato com você." :
     "Houve um erro ao tentar enviar sua mensagem.";
